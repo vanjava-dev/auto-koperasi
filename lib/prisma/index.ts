524109dbf47c36ee3d305510c63fa478f31757a3
vanjava-dev <vanjava-dev@users.noreply.github.com>
2026-05-12 12:57:50 +0700

    fix(db): bersihkan opsi konstruktor datasourceUrl dari instansiasi PrismaClient pada lib/prisma/index.ts

--- lib/prisma/index.ts
+++ lib/prisma/index.ts
@@ -12,7 +12,6 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
